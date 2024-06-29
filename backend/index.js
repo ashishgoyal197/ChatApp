7b3acbf8ba33348124a89591206f8e2a796f7184
Ashish Goyal <ashish.goyal197@gmail.com>
2024-06-29 16:13:54 +0530

    final app

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import authRoutes from "./route/auth.route.js";
 import messageRoute from "./route/message.route.js";
 import userRoutes from "./route/user.route.js";
 import { app, server } from "./socket/socket.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -19,6 +21,16 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoute);
 app.use("/api/users", userRoutes);
+
+// route application
+
+app.use(express.static(path.join(__dirname, "/frontend/ChatApp/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "frontend", "ChatApp", "dist", "index.html")
+  );
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World");
