1d8650953fa94b63bfe6f04d98bb1efeadc34311
Ashish Goyal <ashish.goyal197@gmail.com>
2024-07-01 17:58:19 +0530

    update

--- frontend/ChatApp/src/pages/home/Home.jsx
+++ frontend/ChatApp/src/pages/home/Home.jsx
@@ -3,15 +3,15 @@ import MessageContainer from "../../component/messages/MessageContainer";
 import useConversation from "../../zustand/useConversation";
 
 export default function Home() {
-  const { messanger } = useConversation();
-  // console.log(messanger);
+  const { messenger } = useConversation();
+  // console.log(messenger);
 
   let screenWidth = window.screen.width;
   let resultHome;
   if (screenWidth <= 750) {
     resultHome = (
       <div className="flex  h-[450px] md:h-[550px] rounded-lg overflow-hidden mb-5 bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
-        {!messanger ? <Sidebar /> : <MessageContainer />}
+        {!messenger ? <Sidebar /> : <MessageContainer />}
       </div>
     );
   } else {
