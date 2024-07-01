c31490d660cccc1d7c298645488ec3df17b8f4de
Ashish Goyal <ashish.goyal197@gmail.com>
2024-07-01 17:49:25 +0530

    update homepage

--- frontend/ChatApp/src/pages/home/Home.jsx
+++ frontend/ChatApp/src/pages/home/Home.jsx
@@ -1,11 +1,27 @@
 import Sidebar from "../../component/sidebar/Sidebar";
 import MessageContainer from "../../component/messages/MessageContainer";
+import useConversation from "../../zustand/useConversation";
 
 export default function Home() {
-  return (
-    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
-      <Sidebar />
-      <MessageContainer />
-    </div>
-  );
+  const { messanger } = useConversation();
+  // console.log(messanger);
+
+  let screenWidth = window.screen.width;
+  let resultHome;
+  if (screenWidth <= 750) {
+    resultHome = (
+      <div className="flex  h-[450px] md:h-[550px] rounded-lg overflow-hidden mb-5 bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        {!messanger ? <Sidebar /> : <MessageContainer />}
+      </div>
+    );
+  } else {
+    resultHome = (
+      <div className="flex h-[450px] md:h-[550px] rounded-lg overflow-hidden mb-5 bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <Sidebar />
+        <MessageContainer />
+      </div>
+    );
+  }
+
+  return <div>{resultHome}</div>;
 }
